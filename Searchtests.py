3bae76623ac3f1389e3c75ae071b7f4ceccb0fc8
Laura Delgado <laurayineth1239@gmail.com>
2020-10-23 00:16:21 -0500

    Test suit

--- Searchtests.py
+++ Searchtests.py
@@ -2,8 +2,7 @@ import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 
-class HomePageTests(unittest.TestCase):
-
+class SearchTests(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path= r'D:\Documents\Curso de Selenium Platzi\chromedriver.exe')
         driver = self.driver
@@ -11,26 +10,23 @@ class HomePageTests(unittest.TestCase):
         driver.maximize_window()
         driver.implicitly_wait(15)
 
-    def test_search_text_field(self):
-        search_field = self.driver.find_element_by_id("search")
-
-    def test_search_text_field_by_name(self):
-        search_field = self.driver.find_element_by_name("q")
-
-    def test_search_text_field_class_name(self):
-        search_field = self.driver.find_elements_by_class_name("input-text required-entry")
-
-    def test_search_button_enabled(self):
-        button = self.driver.find_elements_by_class_name("button search-button")
-
-    def test_count_of_promo_bar(self):
-        banner_list = self.driver.find_element_by_class_name("promos")   
-        banners = banner_list.find_elements_by_tag_name('img')
-        self.assertEqual(3,len(banners))
-
+    def test_search_tee(self):
+        driver = self.driver
+        search_field = driver.find_element_by_name('q')
+        search_field.clear()
+    
+        search_field.send_keys('tee')
+        search_field.submit()
+    
+    def test_search_salt_shaker(self):
+        driver = self.driver
+        search_field = driver.find_element_by_name('q')
+    
+        search_field.send_keys('salt shaker')
+        search_field.submit()
+    
+        products = driver.find_elements_by_xpath('//*[@id="top"]/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/ul/li/div/h2/a')
+        self.assertEqual(1, len(products))
+    
     def tearDown(self):
         self.driver.quit()
-
-
-if __name__== "__main__":
-    unittest.main(verbosity=2)
